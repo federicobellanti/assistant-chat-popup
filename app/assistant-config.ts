04b6d9c82a3fec728b0bc12b935a6b4ed289f3e6
federicobellanti <federico.blt@hotmail.it>
2025-08-12 19:08:42 +0200

    Add assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,3 @@
-export let assistantId = ""; // set your assistant ID here
-
-if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
-}
+// app/assistant-config.ts
+export const ASSISTANT_ID =
+  process.env.NEXT_PUBLIC_ASSISTANT_ID || "asst_ge9zn6BjdwAyyE7Lcf6DmD4e";
